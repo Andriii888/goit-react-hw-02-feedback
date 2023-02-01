174d22912d753e23e06e2ed9d972dbf74d8fc457
Andriii888 <106496840+Andriii888@users.noreply.github.com>
2023-02-02 01:27:35 +0300

    addpravka

--- src/components/feedback/FeedBackOptions/FeedbackOptions.jsx
+++ src/components/feedback/FeedBackOptions/FeedbackOptions.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(option => {
     return (
-      <button key={options.indexOf(option)}type="button" value="good" onClick={onLeaveFeedback}>
+      <button key={options.indexOf(option)}type="button" value={option} onClick={onLeaveFeedback}>
         {option}
       </button>
     );
